82bed573db94f8b8f0e2ae955d4c89ce783e12f9
nukiz <61660487+nukiz@users.noreply.github.com>
2023-12-17 00:26:42 +0200

    [Fabric] TitleScreen - Button improvements

--- src/main/java/me/earth/earthhack/impl/core/mixins/gui/MixinTitleScreen.java
+++ src/main/java/me/earth/earthhack/impl/core/mixins/gui/MixinTitleScreen.java
@@ -2,13 +2,11 @@ package me.earth.earthhack.impl.core.mixins.gui;
 
 import me.earth.earthhack.api.util.interfaces.Globals;
 import me.earth.earthhack.impl.commands.gui.CommandGui;
-import me.earth.earthhack.impl.commands.gui.EarthhackButton;
-import me.earth.earthhack.impl.gui.buttons.SimpleButton;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
-import net.minecraft.client.gui.tooltip.Tooltip;
-import net.minecraft.client.gui.widget.ButtonWidget;
+import net.minecraft.client.gui.widget.TexturedButtonWidget;
 import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -22,8 +20,9 @@ public abstract class MixinTitleScreen extends Screen implements Globals
         super(Text.of("3arthh4ck"));
     }
 
+
     @Unique
-    private ButtonWidget earthhackButton;
+    private static final Identifier EARTHHACK_TEXTURES = new Identifier("earthhack:textures/gui/gui_textures.png");
 
     @Inject(
             method = "init",
@@ -36,14 +35,15 @@ public abstract class MixinTitleScreen extends Screen implements Globals
     public void buttonHook(CallbackInfo info)
     {
         TitleScreen _this = TitleScreen.class.cast(this);
-
-        earthhackButton = ButtonWidget.builder(Text.literal("3arthh4ck"),
-                button -> mc.setScreen(new CommandGui(_this, 2500)))
-                .tooltip(Tooltip.of(Text.literal("Opens the 3arthh4ck Command GUI.")))
-                .dimensions(this.width / 2 + 2 + 98 + 4, this.height / 4 + 48 + 72 + 12 - (20 + 4 * 4), 20, 20)
-                .build();
-
-        this.addDrawableChild(earthhackButton);
+        // noinspection ConstantConditions
+        this.addDrawableChild(new TexturedButtonWidget(
+                this.width / 2 + 2 + 98 + 4,
+                this.height / 4 + 48 + 72 + 12 - (20 + 4 * 4),
+                20, 20,
+                0, 40, 20,
+                EARTHHACK_TEXTURES,
+                256, 256,
+                button -> this.client.setScreen(new CommandGui(_this, 2501))));
     }
 
 }
