920547f9e0220d648287b348b3313685947d17a6
Ai2473 <102620545+Ai2473@users.noreply.github.com>
2024-02-11 13:17:56 +0100

    [Fabric] drawGradientRect

--- src/main/java/me/earth/earthhack/impl/util/render/Render2DUtil.java
+++ src/main/java/me/earth/earthhack/impl/util/render/Render2DUtil.java
@@ -4,6 +4,7 @@ import com.mojang.blaze3d.systems.RenderSystem;
 import me.earth.earthhack.api.util.interfaces.Globals;
 import net.minecraft.client.render.*;
 import net.minecraft.client.util.math.MatrixStack;
+import org.joml.Matrix4f;
 
 import java.awt.*;
 
@@ -124,7 +125,6 @@ public class Render2DUtil implements Globals {
     }
 
     public static void drawGradientRect(MatrixStack matrix, float left, float top, float right, float bottom, boolean sideways, int startColor, int endColor) {
-        /*
         float f = (float) (startColor >> 24 & 255) / 255.0F;
         float f1 = (float) (startColor >> 16 & 255) / 255.0F;
         float f2 = (float) (startColor >> 8 & 255) / 255.0F;
@@ -133,6 +133,29 @@ public class Render2DUtil implements Globals {
         float f5 = (float) (endColor >> 16 & 255) / 255.0F;
         float f6 = (float) (endColor >> 8 & 255) / 255.0F;
         float f7 = (float) (endColor & 255) / 255.0F;
+        BufferBuilder bufferBuilder = Tessellator.getInstance().getBuffer();
+        Matrix4f posMatrix = matrix.peek().getPositionMatrix();
+
+        RenderSystem.enableBlend();
+        RenderSystem.defaultBlendFunc();
+        RenderSystem.setShader(GameRenderer::getPositionColorProgram);
+        bufferBuilder.begin(VertexFormat.DrawMode.QUADS, VertexFormats.POSITION_COLOR);
+        if (sideways) {
+            bufferBuilder.vertex(posMatrix, left, top, 0.0F).color(f1, f2, f3, f).next();
+            bufferBuilder.vertex(posMatrix, left, bottom, 0.0F).color(f1, f2, f3, f).next();
+            bufferBuilder.vertex(posMatrix, right, bottom, 0.0F).color(f5, f6, f7, f4).next();
+            bufferBuilder.vertex(posMatrix, right, top, 0.0F).color(f5, f6, f7, f4).next();
+        } else {
+            bufferBuilder.vertex(posMatrix, right, top, 0.0F).color(f1, f2, f3, f).next();
+            bufferBuilder.vertex(posMatrix, left, top, 0.0F).color(f1, f2, f3, f).next();
+            bufferBuilder.vertex(posMatrix, left, bottom, 0.0F).color(f5, f6, f7, f4).next();
+            bufferBuilder.vertex(posMatrix, right, bottom, 0.0F).color(f5, f6, f7, f4).next();
+        }
+        BufferRenderer.drawWithGlobalProgram(bufferBuilder.end());
+        RenderSystem.disableBlend();
+
+
+        /*
         GlStateManager.disableTexture2D();
         GlStateManager.enableBlend();
         GlStateManager.disableAlpha();
