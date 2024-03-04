3f2579a501199fb015c711823b4516fad576c20a
nukiz <61660487+nukiz@users.noreply.github.com>
2024-03-04 10:48:08 +0200

    [Fabric] typo I think

--- src/main/java/me/earth/earthhack/impl/event/events/client/ClientInitEvent.java
+++ src/main/java/me/earth/earthhack/impl/event/events/client/ClientInitEvent.java
@@ -1,12 +1,9 @@
 package me.earth.earthhack.impl.event.events.client;
 
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.RunArgs;
-import net.minecraft.client.realms.RealmsClient;
-import net.minecraft.resource.ResourceReload;
 
 /**
- * Fired when {@link MinecraftClient#onInitFinished(RealmsClient, ResourceReload, RunArgs.QuickPlay)} is called.
+ * Fired when {@link MinecraftClient#onInitFinished(MinecraftClient.LoadingContext)} is called.
  * Lets the client know when {@link net.minecraft.client.option.GameOptions} is accessible, for example.
  * Could probably be done in a better way (where this event is redundant)
  * but this is relatively simple and clean.
