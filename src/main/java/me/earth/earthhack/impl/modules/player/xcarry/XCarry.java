248663e2f3d5686fa944dbdc6f722e9571c279bf
TempleTerry[x64] <123938029+PhilipPanda@users.noreply.github.com>
2024-06-02 22:45:17 +0200

    Update XCarry.java
    Xcarry fix

--- src/main/java/me/earth/earthhack/impl/modules/player/xcarry/XCarry.java
+++ src/main/java/me/earth/earthhack/impl/modules/player/xcarry/XCarry.java
@@ -1,7 +1,9 @@
 package me.earth.earthhack.impl.modules.player.xcarry;
 
+import com.google.common.eventbus.Subscribe;
 import me.earth.earthhack.api.module.Module;
 import me.earth.earthhack.api.module.util.Category;
+import me.earth.earthhack.impl.event.events.network.PacketEvent;
 import me.earth.earthhack.impl.util.client.SimpleData;
 import net.minecraft.network.packet.c2s.play.CloseHandledScreenC2SPacket;
 
@@ -15,14 +17,19 @@ public class XCarry extends Module
                 "your crafting inventory and drag slot."));
     }
 
-    @Override
-    protected void onDisable()
-    {
-        if (mc.player != null)
-        {
-            mc.player.networkHandler.sendPacket(new CloseHandledScreenC2SPacket(
-                    mc.player.playerScreenHandler.syncId));
+    @Subscribe
+    public void onPacketSend(PacketEvent.Send event) {
+        if (event.getPacket() instanceof CloseHandledScreenC2SPacket packet && packet.getSyncId() == mc.player.playerScreenHandler.syncId && checkSlots()) {
+            event.setCancelled(true);
         }
     }
 
+    public boolean checkSlots() {
+        for (int i = 1; i <= 4; ++i) {
+            if (!mc.player.currentScreenHandler.getSlot(i).getStack().isEmpty()) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
