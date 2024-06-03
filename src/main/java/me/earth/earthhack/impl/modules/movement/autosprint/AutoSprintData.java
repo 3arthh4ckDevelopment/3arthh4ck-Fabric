a00323624856264a367fb1e92d23b855643aacd1
nukiz <61660487+nukiz@users.noreply.github.com>
2024-06-03 16:01:15 +0300

    [Fabric] Sprint - oops

--- src/main/java/me/earth/earthhack/impl/modules/movement/autosprint/AutoSprintData.java
+++ src/main/java/me/earth/earthhack/impl/modules/movement/autosprint/AutoSprintData.java
@@ -9,8 +9,6 @@ public class AutoSprintData extends DefaultData<AutoSprint> {
                 Mode for sprinting.
                 - Legit : Legitimate sprint, simulates Keys.
                 - Rage : Obvious AutoSprint. Allows you to sprint into any direction.""");
-        register(module.faceDirection, "Makes you rotate towards the direction " +
-                "you're moving in. Only for AntiCheats that check this, like Grim.");
     }
 
     public String getDescription() {
