5521d1ef324c8a83952dcb4af2980f64d486c9bd
SplashAni <106507600+SplashAni@users.noreply.github.com>
2024-06-03 16:27:55 +0200

    Made all packet types accessible

--- src/main/java/me/earth/earthhack/impl/core/mixins/network/INetworkState.java
+++ src/main/java/me/earth/earthhack/impl/core/mixins/network/INetworkState.java
@@ -4,6 +4,9 @@ import net.minecraft.network.NetworkSide;
 import net.minecraft.network.NetworkState;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.gen.Accessor;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 
@@ -11,4 +14,5 @@ import java.util.Map;
 public interface INetworkState {
     @Accessor(value = "packetHandlers")
     Map<NetworkSide, NetworkState.PacketHandler<?>> getPacketHandlers();
+
 }
