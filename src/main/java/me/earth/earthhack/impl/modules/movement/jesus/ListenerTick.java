e99325ebc3f7f560c4918da990f5909326afc4a6
crazymoose77756 <ryanrogo064@gmail.com>
2024-06-06 14:31:44 -0400

    [Fabric] Jesus

---
+++ src/main/java/me/earth/earthhack/impl/modules/movement/jesus/ListenerTick.java
@@ -0,0 +1,45 @@
+package me.earth.earthhack.impl.modules.movement.jesus;
+
+import me.earth.earthhack.impl.event.events.misc.TickEvent;
+import me.earth.earthhack.impl.event.listeners.ModuleListener;
+import me.earth.earthhack.impl.modules.movement.jesus.mode.JesusMode;
+import me.earth.earthhack.impl.util.math.position.PositionUtil;
+import net.minecraft.entity.Entity;
+
+final class ListenerTick extends ModuleListener<Jesus, TickEvent>
+{
+    public ListenerTick(Jesus module)
+    {
+        super(module, TickEvent.class);
+    }
+
+    @Override
+    public void invoke(TickEvent event)
+    {
+        Entity entity = PositionUtil.getPositionEntity();
+        if (module.timer.passed(800) && entity != null)
+        {
+            if (module.mode.getValue() == JesusMode.Solid)
+            {
+                if (entity.fallDistance > 3.0F)
+                {
+                    return;
+                }
+
+                if ((entity.isInLava()
+                        || entity.isTouchingWater())
+                        && !entity.isSneaking())
+                {
+                    entity.setVelocity(entity.getVelocity().getX(), 0.1D, entity.getVelocity().getZ());
+                    return;
+                }
+
+                if (PositionUtil.inLiquid() && !entity.isSneaking())
+                {
+                    entity.setVelocity(entity.getVelocity().getX(), 0.1D, entity.getVelocity().getZ());
+                }
+            }
+        }
+    }
+
+}
