961e0ebb0d78b664cd78be838b7f96940dfd5fd8
Ai2473 <102620545+Ai2473@users.noreply.github.com>
2024-08-18 19:07:44 +0200

    [Fabric] Bump version

--- src/main/java/me/earth/earthhack/impl/Earthhack.java
+++ src/main/java/me/earth/earthhack/impl/Earthhack.java
@@ -16,7 +16,7 @@ public class Earthhack implements ClientModInitializer {
 
     private static final Logger LOGGER = LogManager.getLogger("3arthh4ck");
     public static final String NAME = "3arthh4ck";
-    public static final String VERSION = "1.0.3";
+    public static final String VERSION = "1.0.4";
     public static long startMS;
 
     @Override
