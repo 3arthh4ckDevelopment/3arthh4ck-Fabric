3c4dd49e5bd2717697e8847b8ff4fe50d1fce8ba
Ai2473 <102620545+Ai2473@users.noreply.github.com>
2024-08-23 02:59:29 +0200

    [Fabric] Accesswidener, mixin and version fix

--- src/main/java/me/earth/earthhack/impl/Earthhack.java
+++ src/main/java/me/earth/earthhack/impl/Earthhack.java
@@ -15,7 +15,7 @@ public class Earthhack implements ClientModInitializer {
 
     private static final Logger LOGGER = LogManager.getLogger("3arthh4ck");
     public static final String NAME = "3arthh4ck";
-    public static final String VERSION = "1.0.4";
+    public static final String VERSION = "1.0.5";
     public static long startMS;
 
     @Override
